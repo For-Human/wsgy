e5496004372f7608cdc06d2024d64cbfa2ff0065
gaoxinge <gaoxx5@gmail.com>
2017-05-08 02:49:31 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='wsgy',
+    version='0.0.1',
+    description='a microframework for wsgy',
+    author='gaoxinge',
+    author_email='gaoxx5@gmail.com',
+    url='https://github.com/For-Human/wsgy',
+    packages=['wsgy'],
+)
